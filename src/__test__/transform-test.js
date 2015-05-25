3dbb4630d722484fde0207b6b6b255909fa4a687
Josh Black <joshuatblack@ufl.edu>
2015-05-25 15:04:08 -0400

    Beginning work on transform update

--- src/__test__/transform-test.js
+++ src/__test__/transform-test.js
@@ -1,131 +1,182 @@
-import { expect } from 'chai';
+import expect from 'expect';
 import transform from '../core/transform';
 import autoprefix from '../vendor/autoprefix';
 
+
 describe('#transform', () => {
-  it('should transform declarations', () => {
-    expect(transform({ display: 'block' })).to.deep.equal([
-      {
-        rule: 'declaration',
-        selector: undefined,
-        property: 'display',
-        value: 'block'
-      }
-    ]);
+  before(() => { global.__DEV__ = true });
+
+  it('should return a declaration from a property', () => {
+    const input = { display: 'block' };
+
+    expect(transform(input))
+      .toEqual([['display', 'block']]);
   });
 
-  it('should transform pseudo elements', () => {
-    const pseudoElem = { before: { content: 'foo', color: 'blue' }};
+  it('should return a ruleset from a pseudo class', () => {
+    const input = { hover: { background: 'black' } };
 
-    expect(transform(pseudoElem)).to.deep.equal([
-      {
-        rule: 'declaration',
-        selector: 'before',
-        property: 'content',
-        value: 'foo'
-      },
-      {
-        rule: 'declaration',
-        selector: 'before',
-        property: 'color',
-        value: 'blue'
-      }
-    ]);
-  });
-
-  it('should transform pseudo classes', () => {
-    const pseudoClass = { hover: { background: 'black' }};
-
-    expect(transform(pseudoClass)).to.deep.equal([
-      {
-        rule: 'declaration',
-        selector: 'hover',
-        property: 'background',
-        value: 'black'
-      }
-    ]);
-  });
-
-  it('should transform @media', () => {
-    const props = {
-      fontSize: {
-        rule: 'media',
-        queries: [
-          ['(min-width: 320px)', '2em'],
-          ['(min-width: 640px)', '4em']
-        ],
-        value: '1em'
-      }
-    };
-
-    expect(transform(props)).to.deep.equal([
-      {
-        rule: 'declaration',
-        selector: undefined,
-        property: 'font-size',
-        value: '1em'
-      },
-      {
-        rule: 'media',
-        selector: undefined,
-        query: '(min-width: 320px)',
-        property: 'font-size',
-        value: '2em'
-      },
-      {
-        rule: 'media',
-        selector: undefined,
-        query: '(min-width: 640px)',
-        property: 'font-size',
-        value: '4em'
-      }
-    ]);
-  });
-
-  it('should transform media rules inside of pseudo classes', () => {
-    const props = {
-      hover: {
-        background: {
-          rule: 'media',
-          queries: [
-            ['(min-width: 320px)', 'blue'],
-            ['(min-width: 640px)', 'red']
-          ],
-          value: 'black'
+    expect(transform(input, 'a'))
+      .toEqual([
+        {
+          selector: 'a:hover',
+          block: [['background', 'black']]
         }
-      }
-    }
-
-    expect(transform(props)).to.deep.equal([
-      {
-        rule: 'declaration',
-        selector: 'hover',
-        property: 'background',
-        value: 'black'
-      },
-      {
-        rule: 'media',
-        selector: 'hover',
-        query: '(min-width: 320px)',
-        property: 'background',
-        value: 'blue' },
-      {
-        rule: 'media',
-        selector: 'hover',
-        query: '(min-width: 640px)',
-        property: 'background',
-        value: 'red'
-      }
-    ]);
+      ]);
   });
 
-  // @media should throw if no default value
-  // it should throw for invalid queries in __DEV__
-  // it should hoist the default value
-  // it should support one query, or multiple queries
+  it('should return a ruleset from a pseudo element', () => {
+    const input = { before: { content: '' } };
 
-  // it should throw if not a valid CSS type
+    expect(transform(input, 'a'))
+      .toEqual([
+        {
+          selector: 'a::before',
+          block: [['content', '']]
+        }
+      ]);
+  });
 
-  // it should transform keyframes
-  // it should transform fontface
+  it('should hyphenate property values that should be hyphenated', () => {
+    const input = { fontSize: '1em' };
+
+    expect(transform(input))
+      .toEqual([['font-size', '1em']]);
+  });
+
+  it('should throw for invalid properties on the component', () => {
+    const input = { foo: 'bar' };
+
+    expect(() => transform(input))
+      .toThrow(/Invalid Property Type `foo` on a Style Component/);
+  })
 });
+
+
+// describe('#transform', () => {
+//   it('should transform declarations', () => {
+//     expect(transform({ display: 'block' })).to.deep.equal([
+//       {
+//         rule: 'declaration',
+//         selector: undefined,
+//         property: 'display',
+//         value: 'block'
+//       }
+//     ]);
+//   });
+
+//   it('should transform pseudo elements', () => {
+//     const pseudoElem = { before: { content: 'foo', color: 'blue' }};
+
+//     expect(transform(pseudoElem)).to.deep.equal([
+//       {
+//         rule: 'declaration',
+//         selector: 'before',
+//         property: 'content',
+//         value: 'foo'
+//       },
+//       {
+//         rule: 'declaration',
+//         selector: 'before',
+//         property: 'color',
+//         value: 'blue'
+//       }
+//     ]);
+//   });
+
+//   it('should transform pseudo classes', () => {
+//     const pseudoClass = { hover: { background: 'black' }};
+
+//     expect(transform(pseudoClass)).to.deep.equal([
+//       {
+//         rule: 'declaration',
+//         selector: 'hover',
+//         property: 'background',
+//         value: 'black'
+//       }
+//     ]);
+//   });
+
+//   it('should transform @media', () => {
+//     const props = {
+//       fontSize: {
+//         rule: 'media',
+//         queries: [
+//           ['(min-width: 320px)', '2em'],
+//           ['(min-width: 640px)', '4em']
+//         ],
+//         value: '1em'
+//       }
+//     };
+
+//     expect(transform(props)).to.deep.equal([
+//       {
+//         rule: 'declaration',
+//         selector: undefined,
+//         property: 'font-size',
+//         value: '1em'
+//       },
+//       {
+//         rule: 'media',
+//         selector: undefined,
+//         query: '(min-width: 320px)',
+//         property: 'font-size',
+//         value: '2em'
+//       },
+//       {
+//         rule: 'media',
+//         selector: undefined,
+//         query: '(min-width: 640px)',
+//         property: 'font-size',
+//         value: '4em'
+//       }
+//     ]);
+//   });
+
+//   it('should transform media rules inside of pseudo classes', () => {
+//     const props = {
+//       hover: {
+//         background: {
+//           rule: 'media',
+//           queries: [
+//             ['(min-width: 320px)', 'blue'],
+//             ['(min-width: 640px)', 'red']
+//           ],
+//           value: 'black'
+//         }
+//       }
+//     }
+
+//     expect(transform(props)).to.deep.equal([
+//       {
+//         rule: 'declaration',
+//         selector: 'hover',
+//         property: 'background',
+//         value: 'black'
+//       },
+//       {
+//         rule: 'media',
+//         selector: 'hover',
+//         query: '(min-width: 320px)',
+//         property: 'background',
+//         value: 'blue' },
+//       {
+//         rule: 'media',
+//         selector: 'hover',
+//         query: '(min-width: 640px)',
+//         property: 'background',
+//         value: 'red'
+//       }
+//     ]);
+//   });
+
+//   // @media should throw if no default value
+//   // it should throw for invalid queries in __DEV__
+//   // it should hoist the default value
+//   // it should support one query, or multiple queries
+
+//   // it should throw if not a valid CSS type
+
+//   // it should transform keyframes
+//   // it should transform fontface
+// });
--- src/core/transform.js
+++ src/core/transform.js
@@ -1,48 +1,90 @@
+import getType from '../utils/getType';
 import hyphenate from '../vendor/hyphenate';
+import invariant from '../vendor/invariant';
 import deepMapKeys from '../utils/deepMapKeys';
-import isMedia from '../utils/typechecks/isMedia';
-import isPseudo from '../utils/typechecks/isPseudo';
+import { pairs } from 'lodash';
 
-export default function transform(props, selector) {
-  const styles = deepMapKeys(props, hyphenate);
+function declaration(key, value) {
+  return [key, value];
+}
 
-  return Object.keys(styles).reduce((p, key) => {
-    if (isPseudo(key)) {
-      return p.concat(...transform(styles[key], key));
-    }
-    else if (isMedia(key, styles[key])) {
-      const { rule, queries, value } = styles[key];
+function block(...declarations) {
+  return [...declarations];
+}
 
-      // Lift default value
-      const declaration = {
-        rule: 'declaration',
-        selector,
-        property: key,
-        value
-      };
+function ruleset(selector, block) {
+  return { selector, block };
+}
 
-      const q = queries.reduce((p, query) => {
-        const [queryString, propertyValue] = query;
+const classifiers = {
+  declaration,
+  pseudoClass(keyword, declarations, className) {
+    return ruleset(`${className}:${keyword}`, pairs(declarations));
+  },
+  pseudoElement(keyword, declarations, className) {
+    return ruleset(`${className}::${keyword}`, pairs(declarations));
+  }
+};
 
-        return p.concat({
-          rule,
-          selector,
-          query: queryString,
-          property: key,
-          value: propertyValue
-        });
+export default function transform(props, className) {
+  const properties = deepMapKeys(props, hyphenate);
 
-      }, []);
+  return Object.keys(properties).reduce((p, key) => {
+    const type = getType(key, properties[key]),
+          classifier = classifiers[type];
 
-      return p.concat(...[declaration, ...q]);
-    }
-    else {
-      return p.concat({
-        rule: 'declaration',
-        selector,
-        property: key,
-        value: styles[key]
-      });
-    }
+    invariant(
+      classifier,
+      'Currently unsupported CSS feature: %s',
+      key
+    );
+
+    return p.concat([classifier(key, properties[key], className)]);
   }, []);
 }
+
+
+
+// export default function transform(props, selector) {
+//   const styles = deepMapKeys(props, hyphenate);
+
+//   return Object.keys(styles).reduce((p, key) => {
+//     if (isPseudo(key)) {
+//       return p.concat(...transform(styles[key], key));
+//     }
+//     else if (isMedia(key, styles[key])) {
+//       const { rule, queries, value } = styles[key];
+
+//       // Lift default value
+//       const declaration = {
+//         rule: 'declaration',
+//         selector,
+//         property: key,
+//         value
+//       };
+
+//       const q = queries.reduce((p, query) => {
+//         const [queryString, propertyValue] = query;
+
+//         return p.concat({
+//           rule,
+//           selector,
+//           query: queryString,
+//           property: key,
+//           value: propertyValue
+//         });
+
+//       }, []);
+
+//       return p.concat(...[declaration, ...q]);
+//     }
+//     else {
+//       return p.concat({
+//         rule: 'declaration',
+//         selector,
+//         property: key,
+//         value: styles[key]
+//       });
+//     }
+//   }, []);
+// }

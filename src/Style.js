e047d7da6de6208e52375627fa2f07a880a32442
Josh Black <joshuatblack@ufl.edu>
2015-05-29 03:48:40 -0400

    Updating Style component so that Style components compose together and only apply to first non-Style child

--- src/Style.js
+++ src/Style.js
@@ -1,18 +1,26 @@
 import { Children, cloneElement } from 'react';
-import { omit } from 'lodash';
+import { isFunction, omit } from 'lodash';
 import getStylesFrom from './core/getStylesFrom';
 
 export default class Style {
   render () {
     const props = Object.assign({}, omit(this.props, 'children'));
+
+    if (Children.count(this.props.children) === 1) {
+      const child = Children.only(this.props.children);
+
+      if (isFunction(child.type) && new child.type() instanceof Style) {
+        return cloneElement(child, { ...props });
+      }
+    }
+
     const { styles, className } = getStylesFrom(props);
 
     return (
       <div>
         <style dangerouslySetInnerHTML={{ __html: styles }} />
         {
-          Children.map(
-            this.props.children,
+          Children.map(this.props.children,
             (child) => cloneElement(child, { className }))
         }
       </div>
--- src/__test__/style-test.js
+++ src/__test__/style-test.js
@@ -3,23 +3,26 @@ import React from 'react/addons';
 import 'core-js';
 import Style from '../Style';
 
-const { addons, Children, render } = React;
+const { addons, Children, render, renderToString } = React;
 const { TestUtils } = addons;
 
 describe('Style', () => {
-  before(() => { global.React = React });
+  before(() => { global.React = React; });
 
   it('should be a React Element', () => {
     expect(TestUtils.isElement(<Style/>)).toEqual(true);
   });
 
   it('should render a basic declaration', () => {
-    const shallowRenderer = TestUtils.createRenderer();
-    shallowRenderer.render(<Style display="block"><h1>Hello</h1></Style>);
-    const result = shallowRenderer.getRenderOutput();
-
-    expect(result.props.children[0]).toEqual(
-      <style dangerouslySetInnerHTML={{ __html: '.a{display:block;}' }} />
-    );
+    expect(renderToString(<Style display="block"><h1>Hello</h1></Style>))
+      .toBe('<div data-reactid=".0" data-react-checksum="-2045695338"><style data-reactid=".0.0">.a{display:block;}</style><h1 class="a" data-reactid=".0.1:$=10:0">Hello</h1></div>');
   });
+
+  it('should pass in styles to children style components', () => {
+    expect(renderToString(
+      <Style display="block"><Style background="black"><div/></Style></Style>))
+      .toBe('<div data-reactid=".1" data-react-checksum="1927098507"><style data-reactid=".1.0">.a{background:black;display:block;}</style><div class="a" data-reactid=".1.1:$=10:0"></div></div>');
+  });
+
+  // it('should pass in styles to children of components that extend Style', () => {});
 });

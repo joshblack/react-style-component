06d2c01cf367423a0187059e1dbaf85fc844e898
Josh Black <joshuatblack@ufl.edu>
2015-05-29 22:08:44 -0400

    Updating example with composed component along with possible helpers for media queries

--- examples/components/App.js
+++ examples/components/App.js
@@ -1,21 +1,24 @@
 import Style from '../../dist/Style';
 import Center from './Center';
 
+const phone = '(min-width: 320px)';
+const tablet = '(min-width: 640px)';
+
+function media(a, b, c) {
+  return { [phone]: a, [tablet]: b, default: c };
+}
+
 export default class App {
   render() {
     return (
-      <Center>
+      <Center width={media('200px', '400px', '100px')} height="150px">
         <Style
           hover={{
             background: 'black',
             color: 'white',
             cursor: 'pointer'
           }}
-          fontSize={{
-            '(min-width: 320px)': '2em',
-            '(min-width: 640px)': '4em',
-            default: '1em'
-          }}
+          fontSize={media('2em', '4em', '1em')}
           transition="0.2s all">
           <h1>Hello Styles!</h1>
         </Style>
--- examples/components/Center.js
+++ examples/components/Center.js
@@ -2,6 +2,12 @@ import Style from '../../dist/Style';
 
 export default class Center extends Style {
   static defaultProps = {
-    position: 'absolute'
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    margin: 'auto',
+    textAlign: 'center',
   }
 }
